13d7465966f375161e322a507ac1bbf9e9c55a6c
Filipe Fusco <filipe.fusco@gmail.com>
2019-04-23 20:04:49 -0300

    wired up handlers for get and post method

--- main.go
+++ main.go
@@ -6,5 +6,21 @@ import (
 )
 
 func main(){
+	http.HandleFunc("/patient", func (w http.ResponseWriter, r *http.Request){
+		switch r.Method{
+		case "GET":
+			getData(w, r)
+		case "POST":
+			postData(w, r)
+		}
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getData(w http.ResponseWriter, r *http.Request){
+	//do something
+}
+
+func postData(w http.ResponseWriter, r *http.Request){
+	//do something
 }
